455ae1b302a73dae3bb4ba4ba16cb6675d729c47
kaur5003 <46930469+kaur5003@users.noreply.github.com>
2020-07-29 22:35:01 -0400

    Add files via upload

---
+++ assets/data/myCars.ts
@@ -0,0 +1,9 @@
+import { CarInfo } from '../../app/carInfo';
+
+
+export const MYCARS : CarInfo[]  = [
+    { carName:'Jaguar',carModel:'2020 XE',bodyType:'Sedan',color:'Red',picture:'assets/images/car.jpg'},
+        {carName:'Hummer',carModel:'2000AM',bodyType:'SUV',color:'Black',picture:'assets/images/hummer.jpg'},
+    {carName:'Range Rover',carModel:'2020',bodyType:'SUV',color:'Black',picture:'assets/images/car.jpg'},
+    {carName:'Cadillac',carModel:'370Z',bodyType:'SUV',color:'Black',picture:'assets/images/car.jpg'}
+];
